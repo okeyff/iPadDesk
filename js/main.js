b0ae4906c1a2578992c816bd91357b408f1232da
Ruslan <46057513+okeyff@users.noreply.github.com>
2020-05-04 01:05:00 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,6 +6,57 @@ document.getElementById('start-btn').onclick = function () {
     contentPage.style.display = 'block';
 }
 
+// $(document).ready(function(){
+//     $('.right-side').slick({
+//       setting-name: setting-value
+//     });
+//   });
 
+$('.icons-bar').slick({
+    infinite: false,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 2048,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
 
-
+$('.right-side').slick({
+    infinite: false,
+    arrows: false,
+    speed: 500,
+    rows: 3,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 2048,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
